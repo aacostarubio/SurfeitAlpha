cfaf9ace1a92f1803c0ed11c92fc6e74a3f09b98
Alejandro Acosta-Rubio <aacostarubio@gmail.com>
2012-11-10 00:28:53 -0600

    Changed form_for to form_tag because we didnt need it that broad

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -84,4 +84,3 @@ class UsersController < ApplicationController
     end
   end
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :name, :email
-  validates_uniqueness_of :email
-  
+  validates_uniqueness_of :email, :username
+
+
 end
