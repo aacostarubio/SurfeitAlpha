b8b68c618e5e2b8c970346a2efb99f85d1ce12d0
Brian Martinek <brian@brianmartinek.com>
2012-11-15 16:04:07 -0600

    updated new user form with email opt-in, migrated new boolean column for users email preference, formatting form to match wireframe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110081415) do
+ActiveRecord::Schema.define(:version => 20121115215735) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20121110081415) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "username"
+    t.boolean  "email_opt_in"
   end
 
 end
